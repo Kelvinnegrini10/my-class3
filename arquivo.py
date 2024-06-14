28be64f8ef6c4d0a5d90198b205e25707377c1ab
Kelvinnegrini10 <166623008+Kelvinnegrini10@users.noreply.github.com>
2024-06-14 14:33:17 -0300

    Update arquivo.py

--- arquivo.py
+++ arquivo.py
@@ -3,37 +3,37 @@ import time
 import customtkinter
 from PIL import Image, ImageTk
 
-# Configuração do CustomTkinter
+
 customtkinter.set_appearance_mode("dark") 
 customtkinter.set_default_color_theme("dark-blue")
 
-# Criação da janela
+
 janela = customtkinter.CTk() 
-janela.geometry("500x400")
+janela.state("zoomed")  
 janela.title("My Class") 
 
-# Carregamento da imagem
+
 imagem = Image.open("imagem1.png")  
-imagem = imagem.resize((100, 100))  
+imagem = imagem.resize((200, 200))  
 imagem_tk = ImageTk.PhotoImage(imagem)
 
-# Componentes da interface
-logo = customtkinter.CTkLabel(janela, image=imagem_tk, text="") 
-logo.pack(padx=10, pady=10)
 
-prof = customtkinter.CTkEntry(janela, placeholder_text="Nome do professor(a)") 
-prof.pack(padx=10, pady=10)
+logo = customtkinter.CTkLabel(janela, image=imagem_tk, text="", font=("Arial", 24)) 
+logo.pack(padx=20, pady=20)
 
-turma = customtkinter.CTkOptionMenu(janela, values=["Turma A", "Turma B"]) 
-turma.pack(padx=10, pady=10)
+prof = customtkinter.CTkEntry(janela, placeholder_text="Nome do professor(a)", width=400, height=40, font=("Arial", 24))  
+prof.pack(padx=20, pady=20)
 
-periodo = customtkinter.CTkOptionMenu(janela, values=["Manhã", "Tarde"]) 
-periodo.pack(padx=10, pady=10)
+turma = customtkinter.CTkOptionMenu(janela, values=["Turma A", "Turma B"], width=400, height=40, font=("Arial", 24)) 
+turma.pack(padx=20, pady=20)
 
-sala = customtkinter.CTkEntry(janela, placeholder_text="Insira a sala") 
-sala.pack(padx=10, pady=10)
+periodo = customtkinter.CTkOptionMenu(janela, values=["Manhã", "Tarde"], width=400, height=40, font=("Arial", 24)) 
+periodo.pack(padx=20, pady=20)
 
-# Função para enviar mensagem
+sala = customtkinter.CTkEntry(janela, placeholder_text="Insira a sala", width=400, height=40, font=("Arial", 24))  
+sala.pack(padx=20, pady=20)
+
+
 def enviar_mensagem():
     valor_prof = prof.get()
     valor_turma = turma.get()
@@ -42,7 +42,7 @@ def enviar_mensagem():
     
     mensagem = f"Aula do professor(a): {valor_prof}\nTurma: {valor_turma}\nPeríodo: {valor_periodo}\nSala: {valor_sala}"
     
-    # Números de telefone dos alunos das turmas
+    
     turma_A = [
         "+5516992787899", "+5519992874556", "+5516993096589", "+5511956397841", "+5516974033367", "+5516994290154",
         "+5516994290188", "+5516997500763", "+5516988156727", "+5516988016776", "+5516996151754", "+5516933008761",
@@ -55,18 +55,18 @@ def enviar_mensagem():
         "+5516988184175", "+5516997614041"
     ]
 
-    # Definindo os números de telefone de acordo com a turma selecionada
+   
     numeros = turma_A if valor_turma == "Turma A" else turma_B
     
-    # Envio da mensagem para cada número
+  
     for numero in numeros:
         kit.sendwhatmsg_instantly(numero, mensagem, wait_time=10, tab_close=True)
         print(f"Mensagem enviada para {numero}")
-        # Aguarda alguns segundos para evitar bloqueio
+        
         time.sleep(8)
 
-# Botão para enviar mensagem
-botao = customtkinter.CTkButton(janela, text="Enviar notificação", command=enviar_mensagem) 
-botao.pack(padx=10, pady=10)
+
+botao = customtkinter.CTkButton(janela, text="Enviar notificação", command=enviar_mensagem, width=400, height=40, font=("Arial", 24))  # Aumenta o tamanho do botão
+botao.pack(padx=20, pady=20)
 
 janela.mainloop()
